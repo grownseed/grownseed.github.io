2599ddb5e690efb08b9dccb99636f57c3d2932e4
grownseed <hj@grownseed.net>
2016-05-26 20:51:28 -0700

    Animate mandelbrot colors and some optimizations

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -1,30 +1,36 @@
+var colors,
+  hue = 0,
+  width,
+  height,
+  canvas,
+  context,
+  image_data,
+  resolution = 4,
+  offset = resolution / 2,
+  iterations = 100,
+  res_width,
+  res_height,
+
+  one_third = 1/3,
+  two_thirds = 2/3,
+  one_sixth = 1/6;
+
 // http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
-function hue2rgb(p, q, t) {
+function hue2rgb(t) {
+  var p = .25, q = .75;
   if(t < 0) t += 1;
   if(t > 1) t -= 1;
-  if(t < 1/6) return p + (q - p) * 6 * t;
-  if(t < 1/2) return q;
-  if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  if(t < one_sixth) return p + 3 * t;
+  if(t < .5) return q;
+  if(t < two_thirds) return .25 + 3 * (two_thirds - t);
   return p;
 }
 
-function hsl2rgb(h, s, l){
-  var r, g, b;
-
-  if(s == 0){
-    r = g = b = l; // achromatic
-  }else{
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1/3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1/3);
-  }
-
+function hsl2rgb(h){
   return [
-    Math.round(r * 255),
-    Math.round(g * 255),
-    Math.round(b * 255),
+    hue2rgb(h + one_third) * 255,
+    hue2rgb(h) * 255,
+    hue2rgb(h - one_third) * 255,
     255
   ];
 }
@@ -32,25 +38,25 @@ function hsl2rgb(h, s, l){
 function drawMandelbrot() {
   var x, y, i, xt,
     cx, cy,
-    zx, zy, zsum,
-    width = window.innerWidth,
-    height = window.innerHeight,
-    canvas = document.getElementById('surface'),
-    context = canvas.getContext('2d'),
-    image_data = context.getImageData(0, 0, width, height),
-    resolution = 4,
-    offset = resolution / 2,
-    res_width = resolution / width,
-    res_height = resolution / height,
-    iterations = 100;
+    zx, zy, zsum;
+
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas = document.getElementById('surface');
+  context = canvas.getContext('2d');
+  image_data = context.getImageData(0, 0, width, height);
+  res_width = resolution / width;
+  res_height = resolution / height;
+
+  colors = {};
 
   canvas.width = width;
   canvas.height = height;
 
-  for(x = 0; x < width; x++) {
+  for (x = 0; x < width; x++) {
     cx = -offset + x * res_width;
 
-    for(y = 0; y < height; y++) {
+    for (y = 0; y < height; y++) {
       i = 0;
       cy = -offset + y * res_height;
       zx = 0;
@@ -65,18 +71,39 @@ function drawMandelbrot() {
       }
       while(i < iterations && zsum < 4);
 
-      var index = ((y * width + x) * 4),
-        rgb = (zsum > 4 ? hsl2rgb(((i * 50 * zx / zy) % 360) / 360, .5, .5) : [0, 0, 0, 255]);
+      var index = ((y * width + x) * 4);
 
-      image_data.data[index] = rgb[0];
-      image_data.data[++index] = rgb[1];
-      image_data.data[++index] = rgb[2];
-      image_data.data[++index] = rgb[3];
+      if (zsum > 4) {
+        colors[index] = (i * 50 * zx / zy);
+      }else{
+        image_data.data[index] = 0;
+        image_data.data[++index] = 0;
+        image_data.data[++index] = 0;
+        image_data.data[++index] = 255;
+      }
     }
+  }
+}
+
+function rotateHue() {
+  hue += 2;
+
+  for (var index in colors) {
+    var rgb = hsl2rgb(((colors[index] + hue) % 360) / 360);
+
+    image_data.data[index] = rgb[0];
+    image_data.data[++index] = rgb[1];
+    image_data.data[++index] = rgb[2];
+    image_data.data[++index] = rgb[3];
   }
 
   context.putImageData(image_data, 0, 0);
+
+  return setTimeout(rotateHue, 50);
 }
 
-document.addEventListener('DOMContentLoaded', drawMandelbrot);
+document.addEventListener('DOMContentLoaded', function() {
+  drawMandelbrot();
+  rotateHue();
+});
 window.addEventListener('resize', drawMandelbrot);
