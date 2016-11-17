07d10acc32a959422c1d13edda66f9e73a48c4e7
Hadrien Jouet <hjouet@bcgsc.ca>
2016-11-17 15:45:42 -0800

    Add text ascii binary relation drawer

---
+++ tr/tr.js
@@ -0,0 +1,48 @@
+let source_input,
+  canvas;
+
+const charmap = {
+  '00': '─',
+  '01': '┬',
+  '11': '┼',
+  '10': '┴'
+};
+
+function parseSource() {
+  let bin = [...source_input.value].map(c => {
+    let d = c.charCodeAt(0).toString(2);
+    return '0'.repeat(8 - d.length) + d;
+  });
+
+  render(bin);
+}
+
+function render(bin = []) {
+  if (bin.length < 2) {
+    canvas.innerText = '';
+    return;
+  }
+
+  let out = [];
+
+  for (const [i, b] of bin.entries()) {
+    if (bin[i + 1]) {
+      let str = '';
+      for (const [j, c] of [...bin[i]].entries()) {
+        str += charmap[c + bin[i + 1][j]];
+      }
+      out.push(str);
+    }
+  }
+
+  canvas.innerText = out.join("\n");
+}
+
+function init() {
+  source_input = document.getElementById('source');
+  canvas = document.getElementById('canvas');
+
+  source_input.addEventListener('keyup', parseSource);
+}
+
+document.addEventListener('DOMContentLoaded', init);
